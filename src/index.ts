b409ca4be5861d47ed915704ce02943fcfa0dce3
David Perez Alvarez <davidpa9708@gmail.com>
2018-11-06 21:18:22 -0500

    feat: add names
    It creates names in nested data components

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import dynamicOnChange from "dynamic-on-change";
+import getNames, { names } from "./getNames";
 
 export class HandleDataChange<D> {
   private _Data: D;
-  constructor(data: D, private _onChange: (newData: D) => void) {
+  constructor(
+    data: D,
+    private _onChange: (newData: D) => void,
+    name?: string,
+    prefix?: string
+  ) {
     this._Data = Object.assign({}, data);
+    this.name = getNames<D>(name || "data", prefix || ".");
   }
   public change = dynamicOnChange<Required<D>>((key, value) => {
     this._Data[key] = value;
     this._onChange(this._Data);
   });
+  public name: names<D>;
 }
 
 export default HandleDataChange;
